1e6a1914420f44ab9e8fe27f850ba9b1fe7066d4
Sebastian Bensusan <sbensu@gmail.com>
2023-06-15 16:26:16 -0400

    cleanup main

--- src/main.zig
+++ src/main.zig
@@ -215,47 +215,9 @@ pub fn main() anyerror!void {
     }
 }
 
-fn copyCsv(comptime T: type, from_path: []const u8, to_path: []const u8) !usize {
-    var from_file = try fs.cwd().openFile(from_path, .{});
-    defer from_file.close();
-    const reader = from_file.reader();
-
-    var to_file = try fs.cwd().createFile(to_path, .{});
-    defer to_file.close();
-    const writer = to_file.writer();
-
-    const allocator = std.testing.allocator;
-
-    var arena = std.heap.ArenaAllocator.init(allocator);
-    defer arena.deinit();
-
-    var parser = try parse.CsvParser(T).init(arena.allocator(), reader, .{});
-
-    var serializer = serialize.CsvSerializer(T).init(.{}, writer);
-
-    var rows: usize = 0;
-    try serializer.writeHeader();
-    while (try parser.next()) |row| {
-        rows = rows + 1;
-        try serializer.appendRow(row);
-    }
-
-    return rows;
-}
-test "end to end" {
-    const from_path = "test/data/simple_end_to_end.csv";
-    const to_path = "tmp/simple_end_to_end.csv";
-
-    var from_file = try fs.cwd().openFile(from_path, .{});
-    defer from_file.close();
-
-    var to_file = try fs.cwd().openFile(to_path, .{});
-    defer to_file.close();
-
-    const rows = try copyCsv(Simple, from_path, to_path);
-
-    const expected_rows: usize = 17;
-    try std.testing.expectEqual(expected_rows, rows);
-
-    try std.testing.expect(try utils.eqlFileContents(from_file, to_file));
+test "call all tests" {
+    // _ = @import("end_to_end_test.zig");
+    // _ = @import("serialize.zig");
+    // _ = @import("parse.zig");
+    _ = @import("new_tokenizer.zig");
 }
